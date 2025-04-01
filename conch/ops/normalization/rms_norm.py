88845288000cfce25905176aa65a21500b446437
Jacob Manning <jmanning+oss@stackav.com>
2025-04-01 17:02:48 -0400

    Add RMS norm kernel (Llama variant)

---
+++ conch/ops/normalization/rms_norm.py
@@ -0,0 +1,41 @@
+# Copyright (C) 2025 Stack AV Co. - All Rights Reserved.
+
+"""RMS norm."""
+
+import torch
+
+from conch.kernels.normalization.rms_norm import fused_add_rms_norm_launcher, rms_norm_launcher
+
+
+def rms_norm(x: torch.Tensor, weight: torch.Tensor, epsilon: float) -> torch.Tensor:
+    """Root-mean-square normalization.
+
+    Args:
+        x: Input tensor, of shape (..., hidden_size).
+        weight: Weight tensor, of shape (hidden_size,).
+        epsilon: Epsilon value.
+    """
+    output = torch.empty_like(x, dtype=x.dtype, device=x.device)
+
+    # Call kernel launch wrapper
+    rms_norm_launcher(output, x, weight, epsilon)
+
+    return output
+
+
+def fused_add_rms_norm(
+    x: torch.Tensor,
+    residual: torch.Tensor,
+    weight: torch.Tensor,
+    epsilon: float,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """Root-mean-square normalization with fused add.
+
+    Args:
+        x: Input tensor, of shape (..., hidden_size).
+        residual: Residual tensor, of shape (..., hidden_size).
+        weight: Weight tensor, of shape (hidden_size,).
+        epsilon: Epsilon value.
+    """
+    fused_add_rms_norm_launcher(x, residual, weight, epsilon)
+    return x, residual
